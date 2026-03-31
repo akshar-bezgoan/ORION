0d1619d937630f1e07a3a631fd96529f759bd1ea
Akshar Bezgoan <aksharbezgoan@Akshars-MacBook-Air.local>
2026-03-31 11:20:07 +0100

    Initial commit: Updating file tree

---
+++ CPP Practice/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    const double pi = 3.14;
+    double radius;
+    cout << "Enter the radius of a circle: ";
+    cin >> radius;
+    double area = pi * pow(radius, 2);
+    cout << area;
+    
+    return 0;
+}
