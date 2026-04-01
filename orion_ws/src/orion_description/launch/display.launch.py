e8da4da952083198123676723ca68795b9c6264c
akshar-bezgoan <aksharbezgoan@gmail.com>
2026-04-01 17:31:59 +0100

    Updating orion.urdf and orion.xacro to Model 1

--- orion_ws/src/orion_description/launch/display.launch.py
+++ orion_ws/src/orion_description/launch/display.launch.py
@@ -21,6 +21,7 @@ def generate_launch_description():
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
+            name='robot_state_publisher',
             output='screen',
             parameters=[{
                 'robot_description': robot_description
